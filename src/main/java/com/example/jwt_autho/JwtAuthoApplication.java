7124eeec3dbf9e9f0135140df9444b39f93050ad
Thien Truong <caothien231@gmail.com>
2024-08-20 16:28:37 -0700

    Initial commit

---
+++ src/main/java/com/example/jwt_autho/JwtAuthoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_autho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthoApplication.class, args);
+	}
+
+}
